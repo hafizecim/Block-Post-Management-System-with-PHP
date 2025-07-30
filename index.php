09979f19271e64d95a662c7b107b9467771323bd
hafizecim <hfz.snyl@gmail.com>
2025-07-30 12:10:04 +0300

    index sayfası günceleme

--- index.php
+++ index.php
@@ -12,16 +12,16 @@ $sonuc = $baglanti->query($sql);
     <title>Blog Yazıları</title>
 </head>
 <body>
-    <h1>Blog Yazıları</h1>
-    <a href="ekle.php">Yeni Yazı Ekle</a>
+    <h1 style="text-align: center; font-family: Verdana, sans-serif; color: #F54927;">Blog Yazıları</h1>
+    <a href="ekle.php" style="display: block; text-align: center; color:darkcyan">Yeni Yazı Ekle</a>
     <hr>
 
     <?php while ($satir = $sonuc->fetch_assoc()): ?>
-        <h2><?php echo htmlspecialchars($satir['baslik']); ?></h2>
+        <h2><em><?php echo htmlspecialchars($satir['baslik']); ?></em></h2>
         <p><?php echo nl2br(htmlspecialchars($satir['icerik'])); ?></p>
         <p><em><?php echo $satir['tarih']; ?></em></p>
-        <a href="duzenle.php?id=<?php echo $satir['id']; ?>">Düzenle</a> | 
-        <a href="sil.php?id=<?php echo $satir['id']; ?>" onclick="return confirm('Silmek istediğine emin misin?');">Sil</a>
+        <a href="duzenle.php?id=<?php echo $satir['id']; ?>" style="color: dodgerblue">Düzenle</a> | 
+        <a href="sil.php?id=<?php echo $satir['id']; ?>" onclick="return confirm('Silmek istediğine emin misin?');" style="color: crimson">Sil</a>
         <hr>
     <?php endwhile; ?>
 </body>
